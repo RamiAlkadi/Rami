b88b8ac7ad30e6a19b741ad890bfb020631de441
RamiAlkadi <60614485+RamiAlkadi@users.noreply.github.com>
2020-02-17 13:30:59 +0100

    Add files via upload

---
+++ methods.mjs
@@ -0,0 +1,20 @@
+function fn() {
+    alert("hoi");
+}
+
+function sn() {
+    alert("good?");
+}
+
+function gn() {
+    alert("bye");
+}
+
+function ar() {
+    const list = ["black","green","pink","white"];
+    return list;
+}
+
+// console.log(ar());
+
+export {fn,ar,gn};
